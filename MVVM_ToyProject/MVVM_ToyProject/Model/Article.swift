93cd724fdf262c30aec5dcd601b19f5de3df24f1
Taekhwan Han <hth3396@gmail.com>
2023-02-21 16:25:47 +0900

    feat: 프로젝트 구조 생성

---
+++ MVVM_ToyProject/MVVM_ToyProject/Model/Article.swift
@@ -0,0 +1,12 @@
+//
+//  News.swift
+//  MVVM_ToyProject
+//
+//  Created by 한택환 on 2023/02/21.
+//
+
+import Foundation
+
+struct Article: Decodable {
+    
+}
---
+++ MVVM_ToyProject/MVVM_ToyProject/ViewModel/NewsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  NewsViewModel.swift
+//  MVVM_ToyProject
+//
+//  Created by 한택환 on 2023/02/21.
+//
+
+import Foundation
+
+class NewsViewModel {
+    
+}
