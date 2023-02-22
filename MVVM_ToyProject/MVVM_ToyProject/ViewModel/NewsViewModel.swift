9c4ac32ef0b12784262b6b410c9a0923e8bfe406
Taekhwan Han <hth3396@gmail.com>
2023-02-22 19:34:34 +0900

    feat: ViewModel 구현

--- MVVM_ToyProject/MVVM_ToyProject/ViewModel/NewsViewModel.swift
+++ MVVM_ToyProject/MVVM_ToyProject/ViewModel/NewsViewModel.swift
@@ -7,6 +7,32 @@
 
 import Foundation
 
-class NewsViewModel {
+struct NewsViewModel {
+    let articles: [Article]
+}
+
+extension NewsViewModel {
+    var articleCount: Int {
+        return articles.count
+    }
     
+    func articleIndex(_ i: Int) -> ArticleViewModel {
+        return ArticleViewModel(article: articles[i])
+    }
+}
+
+struct ArticleViewModel {
+    let article: Article
+}
+
+extension ArticleViewModel {
+    var author: String? {
+        return article.author
+    }
+    var title: String? {
+        return article.title
+    }
+    var description: String? {
+        return article.description
+    }
 }
