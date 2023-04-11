6b60da704d42288700728204ed7b53d63e1e893d
Taekhwan Han <hth3396@gmail.com>
2023-04-11 15:24:58 +0900

    feat: NetworkService 수정 URL 추가 및 예외처리 data 없는 경우 예외처리

--- MVVM_ToyProject/MVVM_ToyProject/Services/NetworkService.swift
+++ MVVM_ToyProject/MVVM_ToyProject/Services/NetworkService.swift
@@ -8,17 +8,18 @@
 import Foundation
 
 class NetworkService {
-    func getArticles(url:URL,completion: @escaping ([Article]?)->()){
+    func getArticles(completion: @escaping ([Article]?) -> Void ){
+        guard let url = URL(string: "https://newsapi.org/v2/everything?q=apple&from=2023-04-10&to=2023-04-10&sortBy=popularity&apiKey=1d54e43914a942ccb07949ebf0d00e3d") else { return }
+        
         URLSession.shared.dataTask(with: url){ data , response, error in
             if let error = error {
                 print(error.localizedDescription)
                 completion(nil)
-            } else if let fetchedData = data {
-                let articles = try? JSONDecoder().decode(Articles.self,from:fetchedData)
-                if let articles = articles {
-                    completion(articles.articles)
-                }
-                print(articles?.articles)
+            }
+            guard let data = data else { return }
+            let articles = try? JSONDecoder().decode(Articles.self,from: data )
+            if let articles = articles {
+                completion(articles.articles)
             }
         }.resume()
     }
